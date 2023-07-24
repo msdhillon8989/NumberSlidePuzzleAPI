32e22041efcf0ef01d65e45d3392089d09cd7dab
msdhillon8989 <msdhillon8989@gmail.com>
2023-07-24 10:16:05 +0000

    added logs 1

--- src/main/java/com/game/puzzle/service/HealthCheckController.java
+++ src/main/java/com/game/puzzle/service/HealthCheckController.java
@@ -1,14 +1,8 @@
 package com.game.puzzle.service;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 
 @RestController
@@ -18,4 +12,5 @@ public class HealthCheckController {
     public String getStatus() {
         return "Application is up and running";
     }
+
 }
